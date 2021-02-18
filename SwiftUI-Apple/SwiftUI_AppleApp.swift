94a11d58fdebef3792c3fbeaff58c110e9cc218f
Mayank Rikh <mayankrikh@gmail.com>
2021-02-18 23:15:51 +0000

    Initial Commit

---
+++ SwiftUI-Apple/SwiftUI_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleApp.swift
+//  SwiftUI-Apple
+//
+//  Created by Mayank Rikh on 18/02/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
